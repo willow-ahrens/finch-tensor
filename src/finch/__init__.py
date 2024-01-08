4446323707e048dd12cd8d85a9530f772bbb5feb
Willow Ahrens <willow@csail.mit.edu>
2024-01-07 23:15:48 -0500

    cool!

--- src/finch/__init__.py
+++ src/finch/__init__.py
@@ -1,8 +1,10 @@
 import juliapkg
-juliapkg.add("Finch", "9177782c-1635-4eb9-9bfb-d9dfa25e6bce")
+juliapkg.add("Finch", "9177782c-1635-4eb9-9bfb-d9dfa25e6bce", version="0.6")
 import juliacall
 juliapkg.resolve()
 from juliacall import Main as jl
+
+jl.seval("using Finch")
 
 _plus = jl.seval("Base.:+")
 _minus = jl.seval("Base.:-")
