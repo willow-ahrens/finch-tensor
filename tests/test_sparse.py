45571d5106b8ed5d29f6f85e4f5ab06ad211c081
Mateusz Sokół <mat646@gmail.com>
2024-05-10 10:50:16 +0200

    Add  keyword to asarray

--- tests/test_sparse.py
+++ tests/test_sparse.py
@@ -44,9 +44,10 @@ def test_wrappers(dtype, jl_dtype, order):
 
 @pytest.mark.parametrize("dtype", [np.int64, np.float64, np.complex128])
 @pytest.mark.parametrize("order", ["C", "F", None])
-def test_no_copy_fully_dense(dtype, order, arr3d):
+@pytest.mark.parametrize("copy", [True, False, None])
+def test_no_copy_fully_dense(dtype, order, copy, arr3d):
     arr = np.array(arr3d, dtype=dtype, order=order)
-    arr_finch = finch.Tensor(arr)
+    arr_finch = finch.Tensor(arr, copy=copy)
     arr_todense = arr_finch.todense()
 
     assert_equal(arr_todense, arr)
