fb0272d5aaab3d28b0807aa6ff8f4f2a0b572f01
Mateusz Sokół <mat646@gmail.com>
2024-05-28 14:32:11 +0200

    Add complex functions

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -77,6 +77,19 @@ def test_elemwise_ops_1_arg(arr3d, func_name):
 
 
 @pytest.mark.parametrize(
+    "func_name", ["real", "imag", "conj"]
+)
+def test_elemwise_complex_ops_1_arg(func_name):
+    arr = np.asarray([[1+1j, 2+2j], [3+3j, 4-4j], [-5-5j, -6-6j]])
+    arr_finch = finch.asarray(arr)
+
+    actual = getattr(finch, func_name)(arr_finch)
+    expected = getattr(np, func_name)(arr)
+
+    assert_allclose(actual.todense(), expected)
+
+
+@pytest.mark.parametrize(
     "meth_name",
     ["__pos__", "__neg__", "__abs__", "__invert__"],
 )
