4c04f0f6dfc4f424af5f253c0c9938a13840b5ab
Mateusz Sokół <mat646@gmail.com>
2024-06-12 10:55:48 +0200

    Enable Finch dep from url

--- src/finch/julia.py
+++ src/finch/julia.py
@@ -6,9 +6,15 @@ _FINCH_NAME = "Finch"
 _FINCH_VERSION = "0.6.31"
 _FINCH_HASH = "9177782c-1635-4eb9-9bfb-d9dfa25e6bce"
 _FINCH_REPO_PATH = os.environ.get("FINCH_REPO_PATH", default=None)
+_FINCH_REPO_URL = os.environ.get("FINCH_URL_PATH", default=None)
+
+if _FINCH_REPO_PATH and _FINCH_REPO_URL:
+    raise ValueError("FINCH_REPO_PATH and FINCH_URL_PATH can't be set at the same time.")
 
 if _FINCH_REPO_PATH:  # Also account for empty string
     juliapkg.add(_FINCH_NAME, _FINCH_HASH, path=_FINCH_REPO_PATH, dev=True)
+elif _FINCH_REPO_URL:
+    juliapkg.add(_FINCH_NAME, _FINCH_HASH, url=_FINCH_REPO_URL, dev=True)
 else:
     deps = juliapkg.deps.load_cur_deps()
     if (
